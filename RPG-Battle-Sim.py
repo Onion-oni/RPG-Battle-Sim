7d36cc8074acae4c55c5821697dd7015aa9390d7
Andrew Holmes <167374972+Onion-oni@users.noreply.github.com>
2024-04-29 18:20:36 -0700

    Update RPG-Battle-Sim.py
    Added player class

--- RPG-Battle-Sim.py
+++ RPG-Battle-Sim.py
@@ -1,7 +1,21 @@
 #at least 1 pc and 1 npc
 #need hp, hit, attack, damage, ac, level, modifier
     #user inptus can determine these
+class Player(name):
+
+    def __init__(self):
+#Eventually will have capability to validate user inputs as being integers
+        level = input ("What level is this character? (1-20): ")
+        hitpoints = input("How many hitpoints does this character have?: ")
+        armor = input("What is this character's armor class?: ")
+
+        self.level = int(level)
+        self.hitpoints = int(hitpoints)
+        self.armor = int(armor)
+        self.attack = 0
+        
 #turn order
+    #rolling a d20 determines initiative but I will decide this while testing
 #game logic is to have the two players fight until hp is <= 0
 #need dice for hit and damage (d20, d12, d8, d6, d4)
     #d(x) = dice_maker()
